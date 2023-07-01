e19586379eb758e4ffabf7d88055bb8e763f2b44
unknown <samolazov.roman.alex@gmail.com>
2023-07-01 11:35:09 +0300

    Clear Redux without actionCreate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 
-import store from './store';
+import {createStore} from 'redux';
+import './index.css';
 
-import './index.sass';
-import App from './App';
+const reducer = (state = 0, action) =>{
+    switch (action.type) {
+        case "INC":
+            return state + 1;
+        case "DEC":
+            return state - 1;
+        case "RES":
+            return state = 0;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    document.getElementById('counter').textContent = store.getState();
+});
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({type:'INC'});
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({type:'DEC'});
+});
+
+document.getElementById('res').addEventListener('click', () => {
+    store.dispatch({type:'RES'});
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <>
+        </>
     </React.StrictMode>
 );
